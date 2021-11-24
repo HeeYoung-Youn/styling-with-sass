25a81fb855c8f4a35d080e5154888a8668626b0b
HeeYoung-Youn <yhy1@naver.com>
2021-11-24 11:30:41 +0900

    Add: Sass Button A - size props 설정
    classnames 라이브러리 추가 / size props에 따라 버튼 크기 변경되도록 적용

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
-const Button = ({ children }) => {
-  return <button className="Button">{children}</button>;
+// size: large, medium, small
+const Button = ({ children, size }) => {
+  return <button className={classNames("Button", size)}>{children}</button>;
+};
+
+Button.defaultProps = {
+  size: "medium",
 };
 
 export default Button;
